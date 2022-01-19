5e567d2ea376b6aa1deaf7a23a88c22922f67606
parkerschwantes <23pschwantes@athenian.org>
2022-01-19 14:57:52 -0800

    ALL FINISHED

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -23,8 +23,12 @@ public class MyMain {
     // arithmeticSequence(4) => 9
     // arithmeticSequence(1) => 3
     public static int arithmeticSequence(int n) {
-        // YOUR CODE HERE
-        return -1;
+        if (n == 1){
+            return 3;
+        }
+        else{
+            return arithmeticSequence(n - 1) + 2;
+        }
     }
 
     // Write a method that uses recursion to count the number
@@ -38,8 +42,16 @@ public class MyMain {
     // Hint: to make the subproblem smaller, make the String shorter!
     // Hint 2: multiple recursive cases might be helpful!
     public static int countX(String str) {
-        // YOUR CODE HERE
-        return -1;
+        if (str.length() == 0){
+            return 0;
+        }
+// if doesn't start with x, ignore first letter
+        else if (str.charAt(0) != 'x'){
+            return countX(str.substring(1, str.length()));
+        }
+        else {
+            return 1 + countX(str.substring(1, str.length()));
+        }
     }
 
 
@@ -54,8 +66,15 @@ public class MyMain {
     // exponent(5, 2) = 5 * 5 = 25
     // exponent(4, 0) = 1
     public static int exponent(int base, int power) {
-        // YOUR CODE HERE
-        return -1;
+        if (power == 0){
+            return 1;
+        }
+        else {
+            power--;
+            return base * exponent(base, power);
+
+        }
+
     }
 
     // Write a method that uses recursion to count the number
@@ -64,8 +83,15 @@ public class MyMain {
     // countHi("hix") => 1
     // countHi("hellohihi") => 2
     public static int countHi(String str) {
-        // YOUR CODE HERE
-        return -1;
+        if (str.length() < 2){
+            return 0;
+        }
+        else if (str.substring(0, 2).equals("hi")){
+            return 1 + countHi(str.substring(2));
+        }
+        else {
+            return countHi(str.substring(1));
+        }
     }
 
 
